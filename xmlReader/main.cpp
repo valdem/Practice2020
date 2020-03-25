f77a5b4ef6f366a70449ebeb1256dc96182fe1e5
Vadim <vadim.fomin.2001@bk.ru>
2020-03-25 23:51:57 +0300

    Update main.cpp

--- xmlReader/main.cpp
+++ xmlReader/main.cpp
@@ -4,40 +4,41 @@
 int main() {
    tinyxml2::XMLDocument doc;
    doc.LoadFile("/Users/vadim/Desktop/testPage/xmlReader/test_report_xml.xml");
-   do {
-       const char* infoName = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("name");
+   tinyxml2::XMLElement* miniPath = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase");
+   tinyxml2::XMLElement* next = miniPath->NextSiblingElement("TestCase");
+   const char* infoName = miniPath->Attribute("name");
+   std::cout<<"Test Case Name - "<<infoName<<std::endl;
+   const char* infoFile = miniPath->Attribute("filename");
+   std::cout<<"Filename - "<<infoFile<<std::endl;
+   const char* infoLine = miniPath->Attribute("line");
+   std::cout<<"Line - "<<infoLine<<std::endl;
+   const char* infoSuccess = miniPath->FirstChildElement("Expression")->Attribute("success");
+   std::cout<<"Success - "<<infoSuccess<<std::endl;
+   const char* infoFailLine = miniPath->FirstChildElement("Expression")->Attribute("line");
+   std::cout<<"Line of fail - "<<infoFailLine<<std::endl;
+   const char* infoPath = miniPath->FirstChildElement("Expression")->Attribute("filename");
+   std::cout<<"Path - "<<infoPath<<std::endl;
+   const char* infoOriginal = miniPath->FirstChildElement("Expression")->FirstChildElement("Original")->GetText();
+   const char* infoExpanded = miniPath->FirstChildElement("Expression")->FirstChildElement("Expanded")->GetText();
+   std::cout<<"Original"<<" - "<<infoOriginal<< std::endl;
+   std::cout<<"Expanded"<<" - "<<infoExpanded<< std::endl;
+   while (next){
+       const char* infoName = next->Attribute("name");
        std::cout<<"Test Case Name - "<<infoName<<std::endl;
-       const char* infoFile = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("filename");
+       const char* infoFile = next->Attribute("filename");
        std::cout<<"Filename - "<<infoFile<<std::endl;
-       const char* infoLine = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("line");
+       const char* infoLine = next->Attribute("line");
        std::cout<<"Line - "<<infoLine<<std::endl;
-       const char* infoSuccess = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("success");
+       const char* infoSuccess = next->FirstChildElement("Expression")->Attribute("success");
        std::cout<<"Success - "<<infoSuccess<<std::endl;
-       const char* infoFailLine = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("line");
+       const char* infoFailLine = next->FirstChildElement("Expression")->Attribute("line");
        std::cout<<"Line of fail - "<<infoFailLine<<std::endl;
-       const char* infoPath = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("filename");
+       const char* infoPath = next->FirstChildElement("Expression")->Attribute("filename");
        std::cout<<"Path - "<<infoPath<<std::endl;
-       const char* infoOriginal = doc.FirstChildElement( "Catch" )->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->FirstChildElement("Original")->GetText();
-       const char* infoExpanded = doc.FirstChildElement( "Catch" )->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->FirstChildElement("Expanded")->GetText();
+       const char* infoOriginal = next->FirstChildElement("Expression")->FirstChildElement("Original")->GetText();
+       const char* infoExpanded = next->FirstChildElement("Expression")->FirstChildElement("Expanded")->GetText();
        std::cout<<"Original"<<" - "<<infoOriginal<< std::endl;
        std::cout<<"Expanded"<<" - "<<infoExpanded<< std::endl;
-   }
-   while (doc.FirstChildElement("Catch")->FirstChildElement("Group")->NextSiblingElement("TestCase"));{
-       const char* infoName = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("name");
-       std::cout<<"Test Case Name - "<<infoName<<std::endl;
-       const char* infoFile = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("filename");
-       std::cout<<"Filename - "<<infoFile<<std::endl;
-       const char* infoLine = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->Attribute("line");
-       std::cout<<"Line - "<<infoLine<<std::endl;
-       const char* infoSuccess = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("success");
-       std::cout<<"Success - "<<infoSuccess<<std::endl;
-       const char* infoFailLine = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("line");
-       std::cout<<"Line of fail - "<<infoFailLine<<std::endl;
-       const char* infoPath = doc.FirstChildElement("Catch")->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->Attribute("filename");
-       std::cout<<"Path - "<<infoPath<<std::endl;
-       const char* infoOriginal = doc.FirstChildElement( "Catch" )->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->FirstChildElement("Original")->GetText();
-       const char* infoExpanded = doc.FirstChildElement( "Catch" )->FirstChildElement( "Group" )->FirstChildElement("TestCase")->FirstChildElement("Expression")->FirstChildElement("Expanded")->GetText();
-       std::cout<<"Original"<<" - "<<infoOriginal<< std::endl;
-       std::cout<<"Expanded"<<" - "<<infoExpanded<< std::endl;
+       next = next->NextSiblingElement("TestCase");
    }
 }
